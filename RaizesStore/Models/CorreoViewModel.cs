b15e254b841b7b6af00f964dfe4630b8ecdb3186
eduar <eduar@DESKTOP-ISIK8GV>
2023-11-04 01:37:42 -0600

    Agregar archivos de proyecto.

---
+++ RaizesStore/Models/CorreoViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace RaizesStore.Models
+{
+    public class CorreoViewModel
+    {
+        [DisplayName ("CORREO")]
+        public string correo { get; set; }
+        [DisplayName("TU NOMBRE")]
+        public string nombre { get; set; }
+        [DisplayName("TELEFONO CELULAR")]
+        public string telefono { get; set; }
+        [DisplayName("TITULO DEL CORREO")]
+        public string asunto { get; set; }
+        [DisplayName("DANOS TU OPINON")]
+        public string cuerpo { get; set; }  
+    }
+}
